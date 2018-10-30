2585748c0cac6d9b6ab91a0dccb0e6a546bb46cb
fangyuan2015 <546219618@qq.com>
2018-10-30 17:46:53 +0800

    file opration practice

---
+++ file11.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import os
+
+document = open("testfile.txt","w+")
+print "文件名：",document.name
+document.write("这是我创建的第一个测试文件！\nwelcome!")
+print document.tell()
+#输出当前指针
+document.seek(os.SEEK_SET)
+#设置指针回到文件最初
+context = document.read()
+print context
+document.close()
